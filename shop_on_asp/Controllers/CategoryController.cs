f4c10e93abb641d2b57826b44ab543f0ea2a132c
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-01 01:28:16 +0200

    Create category page

--- shop_on_asp/Controllers/CategoryController.cs
+++ shop_on_asp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using shop_on_asp.Data;
+using shop_on_asp.Models;
 
 namespace shop_on_asp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }

837d9b0e0cf1afcbe07031a4585b8aab3144a4da
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-11-30 16:26:37 +0200

    create connection string and make new dbo category

---
+++ shop_on_asp/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace shop_on_asp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}

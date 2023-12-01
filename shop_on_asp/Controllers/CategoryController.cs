2019865a0cd955c9e276682e05ccd045cd325bf7
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-01 22:48:16 +0200

    add category yo db and add server side and UI validation

--- shop_on_asp/Controllers/CategoryController.cs
+++ shop_on_asp/Controllers/CategoryController.cs
@@ -4,22 +4,38 @@ using shop_on_asp.Models;
 
 namespace shop_on_asp.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            List<Category> objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
-        }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
 
-        public IActionResult Create() 
-        { 
-        return View();
-        }
-    }
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", $"The {nameof(obj.DisplayOrder)} can`t exactly match the {nameof(obj.Name)}");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
 }

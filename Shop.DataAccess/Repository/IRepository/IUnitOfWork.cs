7551c50da334afe4a112dc8656aed0ff21bb1e6a
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-15 01:19:29 +0200

    fix IUnitOfWork

--- Shop.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Shop.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace Shop.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
 
         void Save();
     }

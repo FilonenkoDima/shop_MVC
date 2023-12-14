554a2eff850dbe94b15f0f3ffb821eb73c43ed82
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-15 01:12:24 +0200

    implement ProductRepository

---
+++ Shop.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Upadate(Category obj);
+    }
+}
---
+++ Shop.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Shop.DataAccess.Data;
+using Shop.DataAccess.Repository.IRepository;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Upadate(Category obj)
+        {
+            _db.Products.Update();
+        }
+    }
+}

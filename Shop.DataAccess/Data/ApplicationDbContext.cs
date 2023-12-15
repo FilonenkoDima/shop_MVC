1bdcb147cb013fa70884d7f18806a7945c7bcf3d
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-15 16:53:21 +0200

    add foreign key in product to categories

--- Shop.DataAccess/Data/ApplicationDbContext.cs
+++ Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -33,7 +33,8 @@ namespace Shop.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1
                 },
                 new Product
                 {
@@ -45,8 +46,9 @@ namespace Shop.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
-                },
+                    Price100 = 20,
+					CategoryId = 1
+				},
                 new Product
                 {
                     Id = 3,
@@ -57,8 +59,9 @@ namespace Shop.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
-                },
+                    Price100 = 35,
+					CategoryId = 1
+				},
                 new Product
                 {
                     Id = 4,
@@ -69,8 +72,9 @@ namespace Shop.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
-                },
+                    Price100 = 55,
+					CategoryId = 2
+				},
                 new Product
                 {
                     Id = 5,
@@ -81,8 +85,9 @@ namespace Shop.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
-                },
+                    Price100 = 20,
+					CategoryId = 2
+				},
                 new Product
                 {
                     Id = 6,
@@ -93,8 +98,9 @@ namespace Shop.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
-                }
+                    Price100 = 20,
+					CategoryId = 3
+				}
                 );
         }
     }

56122f433ea519155cb92251844c48b1b2f45c1a
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-15 17:02:13 +0200

    add ImageUrl field to Products table

--- Shop.Models/Product.cs
+++ Shop.Models/Product.cs
@@ -42,5 +42,7 @@ namespace Shop.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }

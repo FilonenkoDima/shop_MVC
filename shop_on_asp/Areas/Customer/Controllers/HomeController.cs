3e500f94c33bb6807f5371d77002f30ea3a7db65
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-19 01:12:00 +0200

    add Details page

--- shop_on_asp/Areas/Customer/Controllers/HomeController.cs
+++ shop_on_asp/Areas/Customer/Controllers/HomeController.cs
@@ -7,31 +7,37 @@ namespace shop_on_asp.Areas.Customer.Controllers
 {
 	[Area("Customer")]
 	public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
-        }
+		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+		{
+			_logger = logger;
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperty: "Category");
-            return View(productList);
-        }
+		public IActionResult Index()
+		{
+			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperty: "Category");
+			return View(productList);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Details(int productId)
+		{
+			Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperty: "Category");
+			return View(product);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }

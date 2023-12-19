01b337a24e55a03c0b6761dc0094e3142d77b051
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-19 14:11:12 +0200

    extend IdentityUser

--- Shop.DataAccess/Data/ApplicationDbContext.cs
+++ Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace Shop.DataAccess.Data
 
         public DbSet<Category> Categories {  get; set; }
         public DbSet<Product> Products {  get; set; }
+        public DbSet<ApplicationUser> applicationUsers {  get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

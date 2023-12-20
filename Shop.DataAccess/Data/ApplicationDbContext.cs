2202c486b96e2d77180b71c51f723d343cdd731a
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-20 14:12:35 +0200

    add Company model to Repository anf UnitOfWork methods

--- Shop.DataAccess/Data/ApplicationDbContext.cs
+++ Shop.DataAccess/Data/ApplicationDbContext.cs
@@ -14,7 +14,8 @@ namespace Shop.DataAccess.Data
 
         public DbSet<Category> Categories {  get; set; }
         public DbSet<Product> Products {  get; set; }
-        public DbSet<ApplicationUser> applicationUsers {  get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers {  get; set; }
+        public DbSet<Company> Companies {  get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ Shop.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using Shop.DataAccess.Data;
+using Shop.DataAccess.Repository.IRepository;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repository
+{
+	public class CompanyRepository : Repository<Company>, ICompanyRepository
+	{
+		private ApplicationDbContext _db;
+
+		public CompanyRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+		public void Update(Company obj)
+		{
+			_db.Companies.Update(obj);
+		}
+	}
+}

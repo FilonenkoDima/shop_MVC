b66ac9ff38b120957a265e5468724df477d11e9f
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-21 01:42:39 +0200

    some small view edit and add migration record to company

---
+++ Shop.DataAccess/Migrations/20231220234025_AddCompanyRecords.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Shop.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Tech City", "Tech Solution", "34534534", "12121", "IL", "123 Tech St" },
+                    { 2, "Vid City", "Vivid Books", "87978978976", "55555", "IL", "999 Vid St" },
+                    { 3, "Lala land", "Readers Club", "23234211", "9999", "NY", "777 Main St" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}

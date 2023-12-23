a6a1395fc4ad8e7091dcfb5129dd54a31c14eb9f
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-23 21:08:46 +0200

    add Product to ShoppingCart

--- shop_on_asp/Areas/Customer/Controllers/HomeController.cs
+++ shop_on_asp/Areas/Customer/Controllers/HomeController.cs
@@ -1,49 +1,79 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shop.DataAccess.Repository.IRepository;
 using Shop.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace shop_on_asp.Areas.Customer.Controllers
 {
-	[Area("Customer")]
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly IUnitOfWork _unitOfWork;
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-		{
-			_logger = logger;
-			_unitOfWork = unitOfWork;
-		}
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
 
-		public IActionResult Index()
-		{
-			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperty: "Category");
-			return View(productList);
-		}
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperty: "Category");
+            return View(productList);
+        }
 
-		public IActionResult Details(int productId)
-		{
-			ShoppingCart cart = new ShoppingCart()
-			{
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart cart = new ShoppingCart()
+            {
                 Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperty: "Category"),
-				Count = 1,
-				ProductId = productId
-        };
-			
-			return View(cart);
-		}
+                Count = 1,
+                ProductId = productId
+            };
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+            return View(cart);
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId &&
+            u.ProductId == shoppingCart.ProductId);
+
+            if(cartFromDb != null)
+            {
+                //shopping cart exists
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Upadate(cartFromDb);
+            }
+            else
+            {
+                // add card record
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }

aec4e3a2f5f4e0850da1271a725584b5f26922bd
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-23 21:49:10 +0200

    add Shopping cart view

---
+++ shop_on_asp/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shop_on_asp.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

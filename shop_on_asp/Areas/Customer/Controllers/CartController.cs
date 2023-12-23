935404a625e0e4f5de9e672918723dbab1de59f4
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-23 22:07:42 +0200

    get shopping cart

--- shop_on_asp/Areas/Customer/Controllers/CartController.cs
+++ shop_on_asp/Areas/Customer/Controllers/CartController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccess.Repository.IRepository;
+using Shop.Models.ViewModels;
+using System.Security.Claims;
 
 namespace shop_on_asp.Areas.Customer.Controllers
 {
@@ -7,9 +10,25 @@ namespace shop_on_asp.Areas.Customer.Controllers
     [Authorize]
     public class CartController : Controller
     {
-        public IActionResult Index()
+        public readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                shoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperty: "Product")
+            };
+
+			return View(ShoppingCartVM);
         }
     }
 }

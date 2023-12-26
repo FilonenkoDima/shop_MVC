9ae251e197fae386a2b1bebdcb1630c4c3c6cf1c
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-26 21:41:18 +0200

    add OrderVM and order controller

---
+++ shop_on_asp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccess.Repository.IRepository;
+using Shop.Models;
+
+namespace shop_on_asp.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperty: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}

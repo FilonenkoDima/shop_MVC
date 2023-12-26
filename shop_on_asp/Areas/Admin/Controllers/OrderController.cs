afdfbc9e9a2fa1aebc22772027882d155d8d5940
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-26 22:54:44 +0200

    add functionality to filters in order list

--- shop_on_asp/Areas/Admin/Controllers/OrderController.cs
+++ shop_on_asp/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Shop.DataAccess.Repository.IRepository;
 using Shop.Models;
+using Shop.Utility;
+using System.Diagnostics;
 
 namespace shop_on_asp.Areas.Admin.Controllers
 {
@@ -22,9 +24,28 @@ namespace shop_on_asp.Areas.Admin.Controllers
 		#region API CALLS
 
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperty: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperty: "ApplicationUser").ToList();
+
+			switch (status)
+			{
+				case "pending":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+					break;
+				case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+			}
+
 			return Json(new { data = objOrderHeaders });
 		}
 

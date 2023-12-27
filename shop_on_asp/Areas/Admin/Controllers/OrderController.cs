250c5df22dff39dbcf341989a936b17408d7dc95
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2023-12-28 01:42:47 +0200

    work on ship order

--- shop_on_asp/Areas/Admin/Controllers/OrderController.cs
+++ shop_on_asp/Areas/Admin/Controllers/OrderController.cs
@@ -11,6 +11,7 @@ using System.Security.Claims;
 namespace shop_on_asp.Areas.Admin.Controllers
 {
 	[Area("Admin")]
+	[Authorize]
 	public class OrderController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
@@ -63,6 +64,41 @@ namespace shop_on_asp.Areas.Admin.Controllers
 
 			return RedirectToAction(nameof(Details), new {orderId = orderHeaderFromDb.Id});
 		}
+
+		[HttpPost]
+		[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+		public IActionResult StartProcessing()
+		{
+			_unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+			_unitOfWork.Save();
+			TempData["Success"] = "Order Details Updated Successfully.";
+
+			return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+		}
+
+		[HttpPost]
+		[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+		public IActionResult ShipOrder()
+		{
+			var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+			orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+			orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+			orderHeader.OrderStatus = SD.StatusShipped;
+			orderHeader.ShippingDate = DateTime.Now;
+			if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+			{
+				orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+			}
+
+			_unitOfWork.OrderHeader.Upadate(orderHeader);
+			_unitOfWork.Save();
+			TempData["Success"] = "Order Shipped Successfully.";
+
+			return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+		}
+
+
+
 
 		#region API CALLS
 

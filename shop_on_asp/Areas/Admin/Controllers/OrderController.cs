a4f6b35bc50d4a95379912067a66d8a455189eba
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-02 00:28:38 +0200

    make domain address in code dynamicaly

--- shop_on_asp/Areas/Admin/Controllers/OrderController.cs
+++ shop_on_asp/Areas/Admin/Controllers/OrderController.cs
@@ -139,7 +139,7 @@ namespace shop_on_asp.Areas.Admin.Controllers
 				.GetAll(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperty: "Product");
 
 			//stripe logic
-			var domain = "https://localhost:7287/";
+			var domain = Request.Scheme + "://" + Request.Host.Value + "/";
 			var options = new SessionCreateOptions
 			{
 				SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",

dfac03b3a91f4f8f5132fecbc184ab8dd8bdef8a
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-01-02 23:49:32 +0200

    Add functionality of update users

--- Shop.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
+++ Shop.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -9,5 +9,6 @@ namespace Shop.DataAccess.Repository.IRepository
 {
 	public interface IApplicationUserRepository : IRepository<ApplicationUser>
 	{
+		void Update(ApplicationUser applicationUser);
 	}
 }
--- shop_on_asp/Areas/Admin/Controllers/UserController.cs
+++ shop_on_asp/Areas/Admin/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Shop.DataAccess.Data;
 using Shop.DataAccess.Repository.IRepository;
 using Shop.Models;
+using Shop.Models.ViewModels;
 using Shop.Utility;
 
 namespace shop_on_asp.Areas.Admin.Controllers
@@ -13,10 +16,65 @@ namespace shop_on_asp.Areas.Admin.Controllers
 	[Authorize(Roles = SD.Role_Admin)]
 	public class UserController : Controller
 	{
-		private ApplicationDbContext _db { get; set; }
-		public UserController(ApplicationDbContext db)
+		private readonly ApplicationDbContext _db;
+		private readonly UserManager<IdentityUser> _userManager;
+		public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
 		{
 			_db = db;
+			_userManager = userManager;
+		}
+
+
+		public IActionResult RoleManagment(string userId)
+		{
+			string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == userId).RoleId;
+
+			RoleManagmentVM RoleVM = new RoleManagmentVM()
+			{
+				ApplicationUser = _db.ApplicationUsers.Include(u => u.Company).FirstOrDefault(u => u.Id == userId),
+				RoleList = _db.Roles.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Name
+				}),
+				CompanyList = _db.Companies.Select(i => new SelectListItem
+				{
+					Text = i.Name,
+					Value = i.Id.ToString()
+				}),
+			};
+
+			RoleVM.ApplicationUser.Role = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+			return View(RoleVM);
+		}
+
+		[HttpPost]
+		public IActionResult RoleManagment(RoleManagmentVM roleManagmentVM)
+		{
+			string RoleID = _db.UserRoles.FirstOrDefault(u => u.UserId == roleManagmentVM.ApplicationUser.Id).RoleId;
+			string oldRole = _db.Roles.FirstOrDefault(u => u.Id == RoleID).Name;
+
+			if(roleManagmentVM.ApplicationUser.Role != oldRole)
+			{
+				//a role was upadated
+				ApplicationUser applicationUser = _db.ApplicationUsers.FirstOrDefault(u => u.Id == roleManagmentVM.ApplicationUser.Id);
+				if(roleManagmentVM.ApplicationUser.Role == SD.Role_Company)
+				{
+					applicationUser.CompanyId = roleManagmentVM.ApplicationUser.CompanyId;
+				}
+				if(oldRole == SD.Role_Company)
+				{
+					applicationUser.CompanyId = null;
+				}
+
+				_db.SaveChanges();
+
+				_userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+				_userManager.AddToRoleAsync(applicationUser, roleManagmentVM.ApplicationUser.Role).GetAwaiter().GetResult();
+			}
+
+			return RedirectToAction("Index");
 		}
 
 		public IActionResult Index()
